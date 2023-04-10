dbd6ae09d0e89793164f23fcd8b71c612c068132
Abhishek Pratap Singh <abhishek@Abhisheks-MacBook-Air.local>
2023-04-11 01:01:34 +0530

    route modifi posts to post

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const db = require('./models');
 app.use(express.json());
+app.use(cors());
 
 const postRouter = require('./routes/Posts');
 app.use("/post",postRouter)
